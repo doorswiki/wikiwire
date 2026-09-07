cb6c094cf1ee5369dd4667c5a39a8883f5181ee6
whostacking <whostacking@gmail.com>
2026-09-07 16:25:40 +0800

    feat: import more main page templates

--- scripts/sync-upstream.mjs
+++ scripts/sync-upstream.mjs
@@ -39,9 +39,11 @@ const PAGES = {
   'Template:Icons': 'templates/shared-lang/Icons/Icons.wikitext',
   'Template:DOORS Wiki/styles.css': 'templates/shared-lang/DOORS Wiki/styles.css',
   'Template:DOORS Wiki/Home Navigation': 'templates/shared-lang/DOORS Wiki/Home Navigation.wikitext',
+  'Template:DOORS Wiki/Home Navigation/item': 'templates/shared-lang/DOORS Wiki/Home Navigation/item.wikitext',
   'Template:DOORS Wiki/Featured Articles': 'templates/shared-lang/DOORS Wiki/Featured Articles.wikitext',
   'Template:DOORS Wiki/Fun Facts': 'templates/shared-lang/DOORS Wiki/Fun Facts.wikitext',
   'Template:DOORS Wiki/Socials': 'templates/shared-lang/DOORS Wiki/Socials.wikitext',
+  'Template:DOORS Wiki/Container': 'templates/shared-lang/DOORS Wiki/Container.wikitext',
   'Template:Documentation': 'templates/shared-lang/Documentation/Documentation.wikitext',
   'Template:Documentation/doc': 'templates/shared-lang/Documentation/doc.wikitext',
 
